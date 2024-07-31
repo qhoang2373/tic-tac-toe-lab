c550d559154d252e00fb3d0af102a002aeb781bc
Quan <qhoang2373@gmail.com>
2024-07-30 20:04:18 -0700

    Step 3 Part D Complete

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ console.log(messageEl)
 /*---------------------------- Variables (state) ----------------------------*/
 
 let board = ['board[0]', 'board[1]', 'board[2]', 'board[3]', 'board[4]', 'board[5]', 'board[6]', 'board[7]', 'board[8]']
-let turn
+let turn = 'X'; 'O'
 let winner 
 let tie 
 
